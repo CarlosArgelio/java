6e1d11ba01b9d5cad6875f62aa67b75a1dd728ee
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-04 17:24:00 -0400

    naming

---
+++ src/NamingJava.java
@@ -0,0 +1,15 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int phoneNumber = 10000;
+
+        System.out.println(phoneNumber);
+
+        String $countryName = "Venezuelan"
+        String _backgroundColor = "Green"
+
+        String background_color = "fdsf";
+        // X ==> String background-color = "fdsf";
+
+        int POSITION = -5;
+    }
+}
