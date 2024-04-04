7a7a9b9f016f6b057601107c79dc46fbcb7e1e61
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-04 17:38:04 -0400

    add types

---
+++ src/types/DataTypes.java
@@ -0,0 +1,10 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int n = 1234567890; 
+        byte nB = 1;
+        
+        long nL = 12345678901L;
+
+        double nD = 123.567894;
+    }
+}
