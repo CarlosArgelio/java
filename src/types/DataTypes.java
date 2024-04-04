17ea8ce094960269bc163cf97d9ce329c099b678
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-04 17:48:48 -0400

    types char

--- src/types/DataTypes.java
+++ src/types/DataTypes.java
@@ -2,9 +2,18 @@ public class DataTypes {
     public static void main(String[] args) {
         int n = 1234567890; 
         byte nB = 1;
-        
         long nL = 12345678901L;
 
         double nD = 123.567894;
+        float nF = 12.34F;
+
+        var salary = 1000; // int
+        // pension 3% 
+        var pension = salary * 0.03; // double
+        var totalSalary = salary - pension;
+
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
     }
 }
