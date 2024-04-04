6772ce12ff98e7374770651d6a7a4a6dee82ded4
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-04 15:07:25 -0400

    Declaring Vars

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declaring Variables
+        int speed;
+        speed = 10;
+        System.out.println(speed);
+    
+        int salary = 1000;
+        String emplyeeName = "Carlos Argelio";
+
+        System.out.println(emplyeeName + " have " + salary);
+    }
+}
