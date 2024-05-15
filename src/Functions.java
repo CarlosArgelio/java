693e11b57fbe9fca1cb37743d9a3aba341fb034f
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-15 16:05:04 -0400

    add java docs

--- src/Functions.java
+++ src/Functions.java
@@ -1,12 +1,21 @@
 public class Functions {
 
+    /**
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         double y = 3;
         double area = circleArea(y);
-        System.out.println(area);
-        
+        System.out.println(area);        
     }
 
+    /**
+     * Descripcion: Retorna el area de un circulo
+     * 
+     * @param r Radio del circulo
+     * @return valor de tipo double del area del circulo
+     */
     public static double circleArea(double r) {
         return Math.PI * Math.pow(r, 2);
     }
