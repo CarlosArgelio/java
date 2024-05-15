71e59faded721ff5595f93005433f25ef6fba77e
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-15 15:34:26 -0400

    add logic operation

---
+++ src/LogicOperations.java
@@ -0,0 +1,8 @@
+public class LogicOperations {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+
+        System.out.println("a es igual a b? " + (a == b));
+    }
+}
